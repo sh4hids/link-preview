0fb23ccf799b2ac9af54aa2414ae871d878da27b
MarquesDev <marques.developpement@gmail.com>
2017-09-21 17:32:00 +0200

    feat(response): change response pattern

--- src/main.js
+++ src/main.js
@@ -2,17 +2,46 @@ const cheerio = require("cheerio");
 const request = require("request");
 
 function collectMeta($, url) {
-  var relativeLinks = $("meta");
-  var image = null;
-  var imageWidth = null;
-  var imageHeight = null;
-  var imageType = null;
-  var title = null;
-  var description = null;
-  var siteName = null;
+  // let relativeLinks = $("meta");
+  // let image = null;
+  // let imageWidth = null;
+  // let imageHeight = null;
+  // let imageType = null;
+  // let title = null;
+  // let description = null;
+  // let siteName = null;
 
-  relativeLinks.each(function() {
-    var property = $(this).attr("property");
+
+
+  // relativeLinks.each(function() {
+  //   let property = $(this).attr("property");
+  //   if (property === "og:image" && image === null)
+  //     image = $(this).attr("content");
+  //   else if (property === "og:image:width" && imageWidth === null)
+  //     imageWidth = $(this).attr("content");
+  //   else if (property === "og:image:height" && imageHeight === null)
+  //     imageHeight = $(this).attr("content");
+  //   else if (property === "og:image:type" && imageType === null)
+  //     imageType = $(this).attr("content");
+  //   else if (property === "og:title" && title === null)
+  //     title = $(this).attr("content");
+  //   else if (property === "og:description" && description === null)
+  //     description = $(this).attr("content");
+  //   else if (property === "og:site_name" && siteName === null)
+  //     siteName = $(this).attr("content");
+  // });
+
+  let relativeLinks = $("meta");
+  let image = null;
+  let imageWidth = null;
+  let imageHeight = null;
+  let imageType = null;
+  let title = null;
+  let description = null;
+  let siteName = null;
+
+  relativeLinks.each(function () {
+    let property = $(this).attr("property");
     if (property === "og:image" && image === null)
       image = $(this).attr("content");
     else if (property === "og:image:width" && imageWidth === null)
@@ -28,6 +57,7 @@ function collectMeta($, url) {
     else if (property === "og:site_name" && siteName === null)
       siteName = $(this).attr("content");
   });
+  
   return {
     url,
     title,
@@ -41,7 +71,7 @@ function collectMeta($, url) {
 }
 const linkPreview = url => {
   return new Promise((resolve, reject) => {
-    request(url, function(error, response, body) {
+    request(url, function (error, response, body) {
       if (response.statusCode === 200) {
         resolve(collectMeta(cheerio.load(body), url));
       } else reject({ error, statusCode: response.statusCode });
