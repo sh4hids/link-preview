e7da1abb4a4bc640b724f522bb4e680fe296af1c
MarquesDev <marques.developpement@gmail.com>
2017-09-22 11:01:16 +0200

    fix(errors): handle good errors on linkPreview function

--- src/main.js
+++ src/main.js
@@ -2,35 +2,6 @@ const cheerio = require("cheerio");
 const request = require("request");
 
 function collectMeta($, url) {
-  // let relativeLinks = $("meta");
-  // let image = null;
-  // let imageWidth = null;
-  // let imageHeight = null;
-  // let imageType = null;
-  // let title = null;
-  // let description = null;
-  // let siteName = null;
-
-
-
-  // relativeLinks.each(function() {
-  //   let property = $(this).attr("property");
-  //   if (property === "og:image" && image === null)
-  //     image = $(this).attr("content");
-  //   else if (property === "og:image:width" && imageWidth === null)
-  //     imageWidth = $(this).attr("content");
-  //   else if (property === "og:image:height" && imageHeight === null)
-  //     imageHeight = $(this).attr("content");
-  //   else if (property === "og:image:type" && imageType === null)
-  //     imageType = $(this).attr("content");
-  //   else if (property === "og:title" && title === null)
-  //     title = $(this).attr("content");
-  //   else if (property === "og:description" && description === null)
-  //     description = $(this).attr("content");
-  //   else if (property === "og:site_name" && siteName === null)
-  //     siteName = $(this).attr("content");
-  // });
-
   var relativeLinks = $("meta");
   var image = null;
   var imageWidth = null;
@@ -70,11 +41,11 @@ function collectMeta($, url) {
   };
 }
 const linkPreview = url => {
+  if (!url || url === '') return Promise.reject({ message: 'You must add a valid url' });
   return new Promise((resolve, reject) => {
     request(url, function (error, response, body) {
-      if (response.statusCode === 200) {
-        resolve(collectMeta(cheerio.load(body), url));
-      } else reject({ error, statusCode: response.statusCode });
+      if (response.statusCode === 200) return resolve(collectMeta(cheerio.load(body), url));
+      return reject({ error, statusCode: response.statusCode });
     });
   });
 };
