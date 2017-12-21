1db9d3e7173bc54d79007efc44fe204c39ed44cc
jeromequeyrou <jerome.queyrou@gmail.com>
2017-12-21 13:34:41 +0100

    feat(deepVideo):

--- src/main.js
+++ src/main.js
@@ -6,9 +6,10 @@ const getByProp = ($, property) =>
     .first()
     .attr("content") || null;
 
-function collectMeta($, url) {
+function collectMeta($, url, deepVideo) {
   const ogUrl = getByProp($, "og:url");
-  return {
+
+  const res = {
     url,
     image: getByProp($, "og:image"),
     imageWidth: getByProp($, "og:image:width"),
@@ -17,12 +18,32 @@ function collectMeta($, url) {
     title: getByProp($, "og:title"),
     description: getByProp($, "og:description"),
     siteName: getByProp($, "og:site_name"),
+    ogVideoUrl: getByProp($, "og:video:url"),
     ogUrl,
     youtube: !ogUrl
       ? null
       : ogUrl.indexOf("youtube.com") >= 0 ? `https://youtube.com/embed/${getValue(ogUrl, "v")}` : null
   };
+  if (deepVideo && !res.ogVideoUrl && !res.youtube)
+    return getDeepVideo(
+      getByProp($, "og:description").match(/(http(s)?:\/\/([a-z0-9]+\.)+[a-z0-9]+(\/[a-z0-9]+)?)/gi)
+    ).then(ogVideoUrl => Object.assign(res, { ogVideoUrl: ogVideoUrl.filter(url => !!url)[0] || null }));
+  return Promise.resolve(res);
 }
+
+const getDeepVideo = urls => {
+  if (!urls) return Promise.resolve([null]);
+  return Promise.all(
+    urls.map(url =>
+      linkPreview
+        .makeRequest(url, 10000)
+        .then(
+          ({ response, body }) =>
+            !response || response.statusCode !== 200 ? null : getByProp(cheerio.load(body), "og:video:secure_url")
+        )
+    )
+  );
+};
 
 const getValue = (url, name) => {
   const i = url.indexOf(`${name}=`) + name.length + 1;
@@ -42,16 +63,19 @@ const getError = url => ({
   siteName: null
 });
 
-const linkPreview = (url, timeout = 100000) => {
+function linkPreview(url, deepVideo = false, timeout = 100000) {
   if (!url || url === "") return Promise.reject({ message: "You must add a valid url" });
   if (!url.match(/^http(s)?:\/\/[a-z]+\.[a-z]+(.)+/i)) return Promise.resolve(getError(url));
-  return new Promise((resolve, reject) => {
-    request(url, { timeout: timeout || 100000 }, function(error, response, body) {
-      if (!response) return resolve(getError(url));
-      if (response.statusCode === 200) return resolve(collectMeta(cheerio.load(body), url));
-      return resolve(getError(url));
-    });
+  return linkPreview.makeRequest(url, timeout).then(({ response, body }) => {
+    if (!response) return getError(url);
+    if (response.statusCode === 200) return collectMeta(cheerio.load(body), url, deepVideo);
+    return getError(url);
   });
-};
+}
+
+linkPreview.makeRequest = (url, timeout) =>
+  new Promise((resolve, reject) => {
+    request(url, { timeout: timeout }, (error, response, body) => resolve({ body, response }));
+  });
 
 module.exports = linkPreview;
